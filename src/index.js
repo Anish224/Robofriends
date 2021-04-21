74c675b2525f78c241085067f78f550747146ca4
Anish  Mankotia <anishmankotia@Anishs-MacBook-Pro.local>
2021-04-21 21:21:32 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {robots} from './robots'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import Card from './Card';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+ReactDOM.render(<App />,
   document.getElementById('root')
 );
 
